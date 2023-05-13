fe4886ab6e391dddd5082f0c88942641df59de7e
Denis Rastrepin <rusBeliy161@yandex.ru>
2023-05-13 16:31:33 +0300

    feat: работа с DOM js

---
+++ script/script.js
@@ -0,0 +1,37 @@
+const openedPopupBtn = document.querySelector('#opened-popup-btn');
+const closePopupBtn = document.querySelector('#close-popup-btn');
+const nameInput = document.querySelector('#name-input');
+const textInput = document.querySelector('#text-input');
+const editPopup = document.querySelector('#edit-popup');
+const editForm = document.querySelector('#edit-form');
+const profileTitle = document.querySelector('.profile__title');
+const profileText = document.querySelector('.profile__text');
+
+
+openedPopupBtn.addEventListener('click', function () {
+  openedPopup(editPopup);
+});
+closePopupBtn.addEventListener('click', function () {
+  closePopup(editPopup);
+});
+  
+nameInput.value = profileTitle.textContent;
+textInput.value = profileText.textContent;
+
+
+editForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+
+  profileText.textContent = textInput.value;
+
+  closePopup(editPopup);
+})
+
+function openedPopup(popup) {
+  popup.classList.add('popup_is-opened')
+}
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened')
+}

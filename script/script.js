83c5b22e5a65ed728252f49138db9b88090f65b5
Denis Rastrepin <rusBeliy161@yandex.ru>
2023-05-13 16:54:24 +0300

    fixes: пустые строки в js

--- script/script.js
+++ script/script.js
@@ -6,29 +6,20 @@ const editPopup = document.querySelector('#edit-popup');
 const editForm = document.querySelector('#edit-form');
 const profileTitle = document.querySelector('.profile__title');
 const profileText = document.querySelector('.profile__text');
-
-
 openedPopupBtn.addEventListener('click', function () {
   openedPopup(editPopup);
 });
 closePopupBtn.addEventListener('click', function () {
   closePopup(editPopup);
 });
-  
 nameInput.value = profileTitle.textContent;
 textInput.value = profileText.textContent;
-
-
 editForm.addEventListener('submit', function (evt) {
   evt.preventDefault();
-
   profileTitle.textContent = nameInput.value;
-
   profileText.textContent = textInput.value;
-
   closePopup(editPopup);
 })
-
 function openedPopup(popup) {
   popup.classList.add('popup_is-opened')
 }

6a9fea0000c27a3ef5fe966326ab5514ccc490ca
Denis Rastrepin <rusBeliy161@yandex.ru>
2023-05-15 13:37:28 +0300

    fixes: исправление ошибок html,css,js

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,37 @@
 const openedPopupBtn = document.querySelector('#opened-popup-btn');
-const closePopupBtn = document.querySelector('#close-popup-btn');
+const editPopup = document.querySelector('#edit-popup');
+
 const nameInput = document.querySelector('#name-input');
 const textInput = document.querySelector('#text-input');
-const editPopup = document.querySelector('#edit-popup');
+
 const editForm = document.querySelector('#edit-form');
 const profileTitle = document.querySelector('.profile__title');
 const profileText = document.querySelector('.profile__text');
+
+const closePopupBtn = document.querySelector('#close-popup-btn');
+
+function openedPopup(popup) {
+  popup.classList.add('popup_is-opened')
+};
+
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened')
+};
+
 openedPopupBtn.addEventListener('click', function () {
   openedPopup(editPopup);
 });
+
+nameInput.value = profileTitle.textContent;
+textInput.value = profileText.textContent;
+
 closePopupBtn.addEventListener('click', function () {
   closePopup(editPopup);
 });
-nameInput.value = profileTitle.textContent;
-textInput.value = profileText.textContent;
+
 editForm.addEventListener('submit', function (evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileText.textContent = textInput.value;
   closePopup(editPopup);
-})
-function openedPopup(popup) {
-  popup.classList.add('popup_is-opened')
-}
-function closePopup(popup) {
-  popup.classList.remove('popup_is-opened')
-}
+});
